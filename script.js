e7365331e7750a4a3693c56f9b3d4c3ffe303295
edi0328 <edisonthai0328@gmail.com>
2024-06-10 02:16:18 -0400

    Create grid

---
+++ script.js
@@ -0,0 +1,24 @@
+//Get grid div to add grid blocks
+
+const grid = document.querySelector("#grid");
+
+//Create 16 divs x 16 divs
+for (let i = 0; i < 16; i++) {
+    const blockRow = document.createElement("div");
+    blockRow.setAttribute("class", "gridRow")
+    for (let j = 0; j < 16; j++) {
+        const block = document.createElement("div");
+        block.setAttribute("class", "block");
+        blockRow.appendChild(block);
+    }
+    grid.appendChild(blockRow);
+}
+
+//For hovering effect
+grid.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("block")) {
+        e.target.setAttribute("style", "background: red;")
+        e.stopPropagation();
+    }
+
+})
